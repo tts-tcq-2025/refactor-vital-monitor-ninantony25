495dfc68afbd9686a97861abeda94edb9d94259d
ninantony25 <57633711+ninantony25@users.noreply.github.com>
2025-08-22 10:30:29 +0530

    Update monitor.h

--- monitor.h
+++ monitor.h
@@ -1,3 +1,12 @@
 #pragma once
 
+enum class VitalStatus {
+    OK,
+    TemperatureCritical,
+    PulseRateOutOfRange,
+    OxygenSaturationOutOfRange
+};
+
+VitalStatus checkVitals(float temperature, float pulseRate, float spo2);
+
 int vitalsOk(float temperature, float pulseRate, float spo2);
